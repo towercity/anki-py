111c511d88b889a3fe67f5076a7f3dc83cca2c3a
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-18 14:09:41 -0500

    pull in the card info

--- scripts/change_cards.py
+++ scripts/change_cards.py
@@ -1,4 +1,5 @@
 import sys, os, json
+from pprint import pprint
 
 # Load Anki library
 sys.path.append("anki")
@@ -15,7 +16,7 @@ with open('config.json', 'r') as conf_file:
 # Load the Collection
 col = Collection(cpath, log=True) # Entry point to the API
 
-# for cid in col.findNotes("tag:%s" % change_tag): 
+# for cid in col.findNotes("tag:00change"): 
 #     note = col.getNote(cid)
 #     front =  note.fields[2] # "Front" is the first field of these cards
 #     print(front)
@@ -32,4 +33,18 @@ def change_cards(col, config):
     print('gathering notes...')
 
     # search the database for subs cards tagged to change
-    noteIds = col.findNotes("tag:%s is:new" % tags['change'])
+    noteIds = col.findNotes("tag:%s" % tags['change'])
+
+    # check for blank notes array; if empty, return False
+    if not len(noteIds):
+        print('error: no notes found')
+        return False
+    
+    print("%s notes gathered\ncreating new notes..." % len(noteIds))
+
+    for noteId in noteIds:
+        note = col.getNote(noteId)
+        term = note.fields[5] # pulls in the vocab term from the 'Note' field
+        print(term)
+
+change_cards(col, config)
