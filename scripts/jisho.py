97c8a1a0a2b3652deab5dadbadeed4c7bffd8881
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-18 21:14:39 -0500

    rename functions

--- scripts/jisho.py
+++ scripts/jisho.py
@@ -4,13 +4,10 @@ from pprint import pprint
 
 JISHO_API = "https://jisho.org/api/v1/search/words?keyword="
 
-def get_all(search_term):
+def get_term_all(search_term):
     url = "%s%s" % (JISHO_API, search_term)
     return json.loads(request.urlopen(url).read())["data"]
 
-def get_one(search_term):
+def get_term_one(search_term):
     jish_resp = get_all(search_term)
     return jish_resp[0] 
-
-alls = get_all('red')
-print("%s: all" % len(alls))
