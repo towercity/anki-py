19eb56f87a06daa18e77094aaa26f19a19c10dcb
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-18 21:12:19 -0500

    fix data

--- scripts/jisho.py
+++ scripts/jisho.py
@@ -4,8 +4,13 @@ from pprint import pprint
 
 JISHO_API = "https://jisho.org/api/v1/search/words?keyword="
 
-def get_json(search_term):
+def get_all(search_term):
     url = "%s%s" % (JISHO_API, search_term)
-    return json.loads(request.urlopen(url).read())
+    return json.loads(request.urlopen(url).read())["data"]
 
-pprint(get_json('red'))
+def get_one(search_term):
+    jish_resp = get_all(search_term)
+    return jish_resp[0] 
+
+alls = get_all('red')
+print("%s: all" % len(alls))
