e3d9789808ba17df5a04d1f04b88c1a112ef58de
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-18 21:32:08 -0500

    add get reading function

--- scripts/jisho.py
+++ scripts/jisho.py
@@ -9,5 +9,15 @@ def get_term_all(search_term):
     return json.loads(request.urlopen(url).read())["data"]
 
 def get_term_one(search_term):
-    jish_resp = get_all(search_term)
-    return jish_resp[0] 
+    jisho_resp = get_term_all(search_term)
+    return jisho_resp[0] 
+
+def get_reading(jisho_resp): #returns term with [furigana]
+    term_dict = jisho_resp['japanese'][0]
+    if 'word' in term_dict:
+        return ("%s[%s]" % (term_dict['word'], term_dict['reading']))
+    else:
+        return term_dict['reading']
+
+
+print(get_reading((get_term_one('red'))))
